4817e40210e85aeb492a120bbb7df1867bcd084f
KotaLakshmi <98080053+KotaLakshmi@users.noreply.github.com>
2022-02-07 17:14:03 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,14 @@
+#swapping of two variables
+x=int(input("enter x value:"))
+y=int(input("enter y value:"))
+x,y=y,x
+print(x)
+print(y)
+
+# largest number and smallest number
+a=int(input("enter a value:"))
+b=int(input("enter b value:"))
+c=min(a,b)
+d=max(a,b)
+print(c,"is smallest number")
+print(d,"is largest number")
