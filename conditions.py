bc27db21eefe94ce619ff6e30405569e6f2a8903
KotaLakshmi <98080053+KotaLakshmi@users.noreply.github.com>
2022-02-07 17:12:41 +0530

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,26 @@
+# if condition
+num=3
+if(num>0):
+    print(num,"is greater than 0")
+
+# if-else condition
+num1=5
+num2=6
+if(num1>num2):
+    print(num1,"is graterthan num2")
+else:
+    print(num2,"is greater than num1")
+
+#elseif-condition
+num1=5
+num2=10
+num3=20
+if(num1>num2):
+    print(num1,"is greater than num2")
+elif(num2>num3):
+    print(num2,"is greater than num3")
+else:
+    print(num3,"is greater than num1 and num2")
+
+
+
