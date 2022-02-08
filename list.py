d498e18102b51aa304563cfd9ad6ccc2e9aa61d6
KotaLakshmi <98080053+KotaLakshmi@users.noreply.github.com>
2022-02-08 09:01:36 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,12 @@
+#list use forloop
+list=[11,2,5,18,7]
+for i in range(0,5):
+    print(list[i],end=" ")
+
+# sum of all elements in the list
+list1=[2,3,5,6,8]
+sum=0
+for i in range(0,len(list1)):
+    sum=sum+list[i]
+print("sum of elements in given list is:",sum)
+
