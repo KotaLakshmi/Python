cbbbf13a6db5c053a20eec617c314e71680b486a
KotaLakshmi <98080053+KotaLakshmi@users.noreply.github.com>
2022-02-08 09:01:59 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+dict={1:'ram',2:'jhon',3:'paul'}
+print(dict,"is initial dictionary")
+del dict[3]
+print("delecting a specific key ")
+print(dict)
+# print(dict.keys)
+# print(dict.values)
+# dict[one]="this is a keyword"
+# print(dict[one])
